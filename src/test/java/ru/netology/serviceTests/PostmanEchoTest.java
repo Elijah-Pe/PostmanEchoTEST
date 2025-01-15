303b722553225398427a2b49381ec6fd9d209a30
Ilya <ilia.petro2014@yandex.ru>
2025-01-15 18:37:15 +0400

    Initial commit

---
+++ src/test/java/ru/netology/serviceTests/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.serviceTests ;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void ShouldReturnPostData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello World!")
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .log().body()
+                .body("data", equalTo("Hello World!"))
+        ;
+    }
+}
